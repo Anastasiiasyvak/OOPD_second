4bedf4551192c912df8bb6ed545c459326ebc5ab
Anastasiiasyvak <asyvak@kse.org.ua>
2023-11-27 11:13:13 +0200

    add main and commandline interface

--- main.cpp
+++ main.cpp
@@ -1,6 +1,83 @@
 #include <iostream>
 
+using namespace std;
+
+
+class ConfigReader{
+public:
+    
+};
+
+class BuyingSystem{
+private:
+    void LoadProducts(const string& filePath){
+        ConfigReader configReader;
+    }
+
+public:
+    BuyingSystem(const string& configFilePath){
+        LoadProducts(configFilePath);
+    }
+
+    void ShowAll(){}
+    void AddProduct(){}
+    void Checkout(){}
+    void CreateOrder(){}
+    void ViewOrderHistory(){}
+
+};
+
+
+class CommandLineInterface{
+private:
+public:
+    CommandLineInterface(){}
+
+    void Commands(){
+    string command;
+    while (true) {
+        cout << "Enter\n1)show all\n2)add product\n3)checkout\n4)create orders\n5)view order history\n6)exit\n";
+        cin >> command;
+
+        if (command == "show all"){
+            printf("hello");
+
+        } else if (command == "add product"){
+
+        } else if (command == "checkout"){
+
+        } else if (command == "create orders"){
+
+        } else if (command == "view order history"){
+
+        } else if (command == "exit"){
+            break;
+        } else{
+            cout << "Invalid command" << endl;
+        }
+
+    }
+
+
+
+
+
+}
+
+};
+
+
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    BuyingSystem buyingSystem("/home/nastia/CLionProjects/OOPD_second/file.txt");
+    CommandLineInterface interface;
+
+    string start;
+    cout << "Enter 'start': " << endl;
+    cin >> start;
+
+    if (start == "start"){
+        interface.Commands();
+    }
+
 }
